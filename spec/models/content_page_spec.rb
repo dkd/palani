6bf96e130fa2c02c1d41bf0d992cdcfff86f7cba
dkd-sprenger <stefan.sprenger@dkd.de>
2009-10-14 20:57:10 +0200

    added spec

--- spec/models/content_page_spec.rb
+++ spec/models/content_page_spec.rb
@@ -2,6 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ContentPage do
   
-  it "should be a child of Page"
+  before(:each) do
+    @content_page = ContentPage.new( :title => "Test" )
+  end
+  
+  it "should be a child class of Page" do
+    @content_page.should be_a_kind_of Page
+  end
   
 end
