5e39a2c6a9eecf6ef69bb87a6fe46e2b586947a5
dkd-sprenger <stefan.sprenger@dkd.de>
2009-11-04 14:59:39 +0100

    refactoring

--- app/controllers/admin/content_elements_controller.rb
+++ app/controllers/admin/content_elements_controller.rb
@@ -9,6 +9,8 @@ class Admin::ContentElementsController < ApplicationController
     end
   end
   
+  # POST /admin/pages/:page_id/content_elements                            AJAX
+  #-----------------------------------------------------------------------------
   def create
     @content_element = ContentElement.new(params[:content_element])
     @content_element.from_content_element params[:page_id], params[:content_element_id]
@@ -23,6 +25,20 @@ class Admin::ContentElementsController < ApplicationController
     @content_elements = @page.content_elements.sorted
     render :update do |page|
       page['middle_content'].replace_html :partial => @partial_file
+    end
+  end
+  
+  # GET /admin/pages/:page_id/content_elements/render_type_settings        AJAX
+  #-----------------------------------------------------------------------------
+  def render_type_settings
+    @content_element = ContentElement.find(params[:id])
+    @content_element.update_attributes :element_type => params[:type]
+    @type = Kernel.const_get(params[:type]).find_by_content_element_id(params[:id])
+    render :update do |page|
+      page['type_settings'].replace_html :partial => "/admin/content_elements/edit/settings", 
+                                         :locals => { :fields => @type.edit_fields, 
+                                            :type => "content_element[]#{@type.class.to_s.underscore}" }
+      page['simple_tabs_javascript'].replace_html :partial => "/admin/content_elements/edit/change_settings"
     end
   end
   
--- app/models/content_element_text.rb
+++ app/models/content_element_text.rb
@@ -5,12 +5,31 @@ class ContentElementText < ActiveRecord::Base
   validates_associated :content_element
   validates_presence_of :text
   
+  # returns the icon, that is used for the backend
   def icon
     "icons/content_elements/content_element_text.png"
   end
   
+  # defines the path to the partial, that gets rendered in the body part of a content element in the backend
+  # custom content elements should have a method 'administration_partial' returning the specific partial
   def administration_partial
     "admin/content_elements/types/content_element_text/administration"
   end
+  
+  # the labels will be used as locale keys
+  # required attributes:
+  # * label
+  # * field_type
+  # * value
+  # optional attributes:
+  # * options
+  # available field types:
+  # * text_field
+  # * text_area
+  # returns the input fields and labels, that should be displayed in the "settings" tab, as an hash
+  def edit_fields
+    []
+  end
+
    
 end
