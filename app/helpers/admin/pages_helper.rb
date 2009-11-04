1ae8f9cd66647716432261dbaae43ae37a57530c
dkd-sprenger <stefan.sprenger@dkd.de>
2009-11-04 14:42:42 +0100

    refactored type settings rendering

--- app/helpers/admin/pages_helper.rb
+++ app/helpers/admin/pages_helper.rb
@@ -5,8 +5,4 @@ module Admin::PagesHelper
     page_types.each { |p| p[0] = t "admin.pages.edit.types."+p[0] }
   end
   
-  def render_settings(page)
-    render :partial => "/admin/pages/edit/settings", :locals => { :fields => page.edit_fields, :type => page.class.to_s.underscore }
-  end
-  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,23 @@ module ApplicationHelper
     languages.each { |r| r[0] = t "backend_languages."+r[0] }
   end
   
+  # renders the settings for a given item
+  # the item must be an instance of the specified types below
+  # items available:
+  # * Page
+  # * ContentElement
+  def render_settings(item)
+    if item.is_a? Page
+      render :partial => "/admin/pages/edit/settings", :locals => { :fields => item.edit_fields, :type => item.class.to_s.underscore }
+    elsif item.is_a? ContentElement
+      @type = Kernel.const_get(item.element_type)
+      unless @type.edit_fields.empty?
+        render :partial => "/admin/content_elements/edit/settings", :locals => { :fields => @type.edit_fields,
+                                                            :type => "#{item.class.to_s.underscore}[]#{@type.class.to_s.underscore}"  }
+      else
+        render :partial => "/admin/content_elements/edit/no_settings"
+      end
+    end
+  end
+  
 end
