066d9c6a6b6e3bed36c3ca8265d8f2b419314e81
dkd-sprenger <stefan.sprenger@dkd.de>
2009-11-09 14:58:31 +0100

    display only clean text for administration

--- app/models/content_element_text.rb
+++ app/models/content_element_text.rb
@@ -18,7 +18,7 @@ class ContentElementText < ActiveRecord::Base
   end
   
   def create_clean_text
-    clean_text = text.gsub(/<\/?[^>]*>/, "")
+    self.clean_text = self.text.gsub(/<\/?[^>]*>/, "")
   end
    
 end
