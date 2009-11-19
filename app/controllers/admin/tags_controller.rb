8418a00692ffffc12db24bd8889b0b252afaba6d
dkd-sprenger <stefan.sprenger@dkd.de>
2009-11-19 13:45:05 +0100

    added specs for tags controller

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,6 +1,8 @@
 class Admin::TagsController < ApplicationController
+  
   def index
     @tags = Tag.name_like(params[:q]).collect { |t| { :tag_name => t.name } }
     render :json => @tags.to_json
   end
+  
 end
--- spec/controllers/admin/tags_controller_spec.rb
+++ spec/controllers/admin/tags_controller_spec.rb
@@ -2,13 +2,28 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Admin::TagsController do
 
-  describe "actions" do
+  before(:each) do
+    @tag = mock_model(Tag, :name => "tag")
+    Tag.stub!(:name_like).and_return([@tag])
+    login_admin
+  end
+
+  describe "GET /admin/tags" do
     
-    describe "index" do
-      
-      it "should render all tags found by the search param"
-      it "should return JSON code"
-      
+    it "should be accessible, if we are authenticated" do
+      xhr :get, :index
+      response.should be_success
+    end
+    
+    it "should not be accessible, if we are not authenticated" do
+      public_user
+      xhr :get, :index
+      response.should_not be_success
+    end
+    
+    it "should search tags with the query sent" do
+      Tag.should_receive(:name_like).with("test")
+      xhr :get, :index, :q => "test"
     end
     
   end
